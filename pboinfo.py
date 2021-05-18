306733ea304468d5359ae4cca50c50c7f543ba92
Sami Laine <sami.v.laine@gmail.com>
2021-05-18 18:03:32 +0200

    pboinfo: Wrap in a main function

--- pboinfo.py
+++ pboinfo.py
@@ -21,73 +21,78 @@ def detect_platform():
         else:
             return "linux"
 
-platform=detect_platform()
 
-try:
-    with urllib.request.urlopen(
-            'https://raw.githubusercontent.com/zeusops/mission-templates/'
-            'master/Zeus_yymmdd_Template.Stratis/template_version.txt') as f:
-        data = f.read().decode('utf-8')
-    latest_version=version.parse(data)
-except urllib.error.HTTPError:
-    latest_version=None
+def main():
+    platform=detect_platform()
 
-parser = argparse.ArgumentParser(description="Parse pbo info")
-parser.add_argument('filename', metavar='FILE', help="PBO file to check")
-args = parser.parse_args()
-
-if platform=="windows":
-    missionpath="C:\server\link\mpmissions"
-elif platform=="wsl":
-    missionpath="/mnt/c/server/link/mpmissions"
-else:
-    home = os.environ['HOME']
-    missionpath=f"{home}/link/mpmissions"
-
-missions=pathlib.Path(missionpath)
-
-#filename="Zeus_190920_Petterson.vt7.pbo"
-filename=args.filename
-
-match=re.search("^Zeus_(\d\d\d\d\d\d)_", filename)
-if match:
-    date_string=match.group(1)
     try:
-        filename_date=datetime.strptime(date_string, '%y%m%d') \
-                      .date().isoformat()
-    except ValueError:
-        # Couldn't find a date in filename, this is not necessarily an error
-        filename_date="INVALID FORMAT"
-else:
-    filename_date="INVALID FORMAT"
+        with urllib.request.urlopen(
+                'https://raw.githubusercontent.com/zeusops/mission-templates/'
+                'master/Zeus_yymmdd_Template.Stratis/template_version.txt') as f:
+            data = f.read().decode('utf-8')
+        latest_version=version.parse(data)
+    except urllib.error.HTTPError:
+        latest_version=None
 
-# TODO: Make path portable
-try:
-    output=subprocess.check_output([f"{home}/files/bin/pboinfo", "-j",
-                                   str(missions / filename)], timeout=10)
-except subprocess.CalledProcessError as e:
-    print("Call to pboinfo failed:")
-    print(e.output.decode('utf-8'))
-    sys.exit(1)
-info=json.loads(output)
-#print(info)
-if info['version'] != 'NOTFOUND':
-    if latest_version:
-        pbo_version=version.parse(info['version'])
-        if pbo_version < latest_version:
-            version_info="PBO uses an outdated template version: {}, latest version: {}" \
-                        .format(pbo_version, latest_version)
-        else:
-            version_info="PBO template version is up to date ({})".format(pbo_version)
+    parser = argparse.ArgumentParser(description="Parse pbo info")
+    parser.add_argument('filename', metavar='FILE', help="PBO file to check")
+    args = parser.parse_args()
+
+    if platform=="windows":
+        missionpath="C:\server\link\mpmissions"
+    elif platform=="wsl":
+        missionpath="/mnt/c/server/link/mpmissions"
     else:
-        version_info="Couldn't check latest template version"
-else:
-    version_info="No template version found"
+        home = os.environ['HOME']
+        missionpath=f"{home}/link/mpmissions"
 
-print("""PBO information:
+    missions=pathlib.Path(missionpath)
 
-Date from filename: {}
-Operation name: {}
-Author: {}
-Faction: {}
-{}""".format(filename_date, info['operation'], info['author'], info['faction'], version_info))
+    #filename="Zeus_190920_Petterson.vt7.pbo"
+    filename=args.filename
+
+    match=re.search("^Zeus_(\d\d\d\d\d\d)_", filename)
+    if match:
+        date_string=match.group(1)
+        try:
+            filename_date=datetime.strptime(date_string, '%y%m%d') \
+                          .date().isoformat()
+        except ValueError:
+            # Couldn't find a date in filename, this is not necessarily an error
+            filename_date="INVALID FORMAT"
+    else:
+        filename_date="INVALID FORMAT"
+
+    # TODO: Make path portable
+    try:
+        output=subprocess.check_output([f"{home}/files/bin/pboinfo", "-j",
+                                       str(missions / filename)], timeout=10)
+    except subprocess.CalledProcessError as e:
+        print("Call to pboinfo failed:")
+        print(e.output.decode('utf-8'))
+        sys.exit(1)
+    info=json.loads(output)
+    #print(info)
+    if info['version'] != 'NOTFOUND':
+        if latest_version:
+            pbo_version=version.parse(info['version'])
+            if pbo_version < latest_version:
+                version_info="PBO uses an outdated template version: {}, latest version: {}" \
+                            .format(pbo_version, latest_version)
+            else:
+                version_info="PBO template version is up to date ({})".format(pbo_version)
+        else:
+            version_info="Couldn't check latest template version"
+    else:
+        version_info="No template version found"
+
+    print("""PBO information:
+
+            Date from filename: {}
+            Operation name: {}
+            Author: {}
+            Faction: {}
+            {}""".format(filename_date, info['operation'], info['author'], info['faction'], version_info))
+
+if __name__ == "__main__":
+    main()
