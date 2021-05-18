db0b5b75fda44e927cc1bb7d977ca4e602507dcc
Sami Laine <sami.v.laine@gmail.com>
2021-05-18 22:14:44 +0200

    Move file to remote

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 import pathlib
+import shutil
 import subprocess
 import traceback
 
@@ -36,7 +37,8 @@ class Bot(commands.Bot):
         else:
             missionpath = f"{home}/link/mpmissions"
 
-        self.downloadfolder = pathlib.Path(missionpath)
+        self.local_folder = pathlib.Path(missionpath)
+        self.remote_folder = pathlib.Path(cfg.REMOTE_FOLDER)
 
     async def on_ready(self):
         print("Logged in as")
@@ -57,25 +59,36 @@ class Bot(commands.Bot):
             print(f"#{message.channel}: <{message.author}> {message.content}")
             return
 
+        print("Received a message")
+        # TODO: remove if
         if message.attachments:
             for attachment in message.attachments:
-                ext = attachment.filename.split('.')[-1]
+                name = attachment.filename
+                ext = name.split('.')[-1]
                 if ext != 'pbo':
-                    print(f"{attachment.filename} is not a pbo")
+                    print(f"{name} is not a pbo")
                     await message.channel.send("Not a pbo, not uploading")
                     return
-                outfile = self.downloadfolder / attachment.filename
-                if os.path.isfile(outfile):
-                    print(f"{outfile} exists")
-                    await message.channel.send(f"File {attachment.filename} "
-                                               "already exists!")
-                    return
-                # await message.channel.send(f"{gehock.mention} pls upload")
+                print(f"Handling {name}")
+                path = self.remote_folder or self.local_folder
+                final_path = path / name
+                print("Checking existing file")
+                try:
+                    if os.path.isfile(final_path):
+                        print(f"{final_path} exists")
+                        await message.channel.send(f"File {name} "
+                                                   "already exists!")
+                        return
+                except OSError:
+                    # Will download locally, check the PBO and complain later
+                    pass
+                outfile = self.local_folder / name
                 if await attachment.save(outfile) != attachment.size:
-                    await message.channel.send("Error. Request manual upload "
-                                               "or try again.")
+                    await message.channel.send("Save failed. Request a manual "
+                                               "upload or try again.")
                     return
                 await message.channel.send("Uploaded")
+                print("Saved")
                 try:
                     info = subprocess.check_output(["./pboinfo.py",
                                                     outfile]).decode('utf-8')
@@ -84,6 +97,18 @@ class Bot(commands.Bot):
                     await message.channel.send("Failed to get PBO info")
                 else:
                     await message.channel.send(info)
+                if self.remote_folder:
+                    print("remote_folder set, moving")
+                    try:
+                        shutil.move(outfile, self.remote_folder / name)
+                    except (FileNotFoundError, PermissionError, OSError) as e:
+                        traceback.print_exc()
+                        await message.channel.send(
+                                f"{self.owner.mention}: Failed to move "
+                                f"the file: {e}")
+                    else:
+                        await message.channel.send("File moved")
+                        print("Moved successfully")
 
     def is_me(self, m):
         return m.author == self.user
